d8b45abe23fc691cf44f107f22ede7fcac0a911a
dex0o0 <hassan13854444@gmail.com>
2026-07-31 17:24:17 +0330

    feat(error): add a enum for handle errors
    - add AppError enum for handle my error
    - add Result for error handling

---
+++ src/error.rs
@@ -0,0 +1,25 @@
+use std::io;
+use thiserror::Error;
+
+#[derive(Debug, Error)]
+pub enum AppError {
+    #[error("Error File System / Inpute-Output: {0}")]
+    Io(#[from] io::Error),
+
+    #[error("Network Error With HTTP: {0}")]
+    Network(#[from] reqwest::Error),
+
+    #[error("Server Not Supported From Range Bytes")]
+    RangeNotSupported,
+
+    #[error("Network Card Target Not Found ({0})")]
+    NetcardNotFound(String),
+
+    #[error("Download Cache Invalid")]
+    InvalidCache,
+
+    #[error("Error Connected To Socket:{0}")]
+    IpcError(String),
+}
+
+pub type Result<T> = std::result::Result<T, AppError>;

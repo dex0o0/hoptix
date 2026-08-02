a0e40233f02b9a6a1d6fdc59b05ed57e5036e5da
dex0o0 <hassan13854444@gmail.com>
2026-08-02 19:51:58 +0330

    add two struct for file and chunk and rename fn

--- src/models/request.rs
+++ src/models/request.rs
@@ -1,7 +1,25 @@
+use std::net::IpAddr;
+
 use crate::error::{AppError, Result};
 use reqwest::{self, Client};
 
-pub async fn check_server_support(url: &str) -> Result<u64> {
+#[derive(Debug, Clone)]
+pub struct Chunck {
+    pub id: usize,
+    pub start: u64,
+    pub end: u64,
+    pub downloaded_bytes: u64,
+    pub assigned_ip: Option<IpAddr>,
+}
+
+#[derive(Debug, Clone)]
+pub struct FileInfo {
+    pub url: String,
+    pub size: u64,
+    pub support_range: bool,
+}
+
+pub async fn check_range_support(url: &str) -> Result<u64> {
     let client = Client::new();
     let response = client.head(url).send().await?;
 
